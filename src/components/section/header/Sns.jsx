e5d010341799ec6296cc1ff4514a72bc944b4ccc
47mango <do_hh@naver.com>
2024-12-18 14:53:54 +0900

    ee

--- src/components/section/header/Sns.jsx
+++ src/components/section/header/Sns.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { snsLink } from '../../../data/header'
-import { Link, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 
 const Sns = () => {
